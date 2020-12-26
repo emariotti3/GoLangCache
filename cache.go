b592582b5469f9e84a370a21d6b4e6caf47fee97
emariotti3 <eugeniamariotti93@gmail.com>
2020-12-25 21:55:59 -0300

    First attempt at solution.

--- cache.go
+++ cache.go
@@ -3,7 +3,13 @@ package sample1
 import (
 	"time"
 	"fmt"
+	"sync"
 )
+
+type Message struct {
+	value float64
+	err error
+}
 
 // PriceService is a service that we can use to get prices for the items
 // Calls to this service are expensive (they take time)
@@ -17,43 +23,131 @@ type PriceService interface {
 type TransparentCache struct {
 	actualPriceService PriceService
 	maxAge             time.Duration
-	prices             map[string]float64
+	prices             map[string]ValueTimestampPair
+	lock			   sync.Mutex
 }
 
 func NewTransparentCache(actualPriceService PriceService, maxAge time.Duration) *TransparentCache {
 	return &TransparentCache{
 		actualPriceService: actualPriceService,
 		maxAge:             maxAge,
-		prices:             map[string]float64{},
+		prices:             map[string]ValueTimestampPair{},
+		lock:				sync.Mutex{},
 	}
 }
+
+func (c *TransparentCache) getPriceFor(itemCode string) (float64, error) {
+	priceValue, err := c.actualPriceService.GetPriceFor(itemCode)
+	if err != nil {
+		return 0, fmt.Errorf("getting price from service : %v", err.Error())
+	}
+	c.lock.Lock()
+	_, ok := c.prices[itemCode]
+	if !ok {
+		c.prices[itemCode] = NewValueTimestampPair(priceValue, time.Now())
+	}
+	c.lock.Unlock()
+
+	return priceValue, nil
+}
+
+type Timestamp struct {
+	timestamp	time.Time
+}
+
+type ValueTimestampPair struct {
+	value		float64
+	timestamp	*Timestamp
+	lock		*sync.RWMutex
+}
+
+func NewValueTimestampPair(value float64, time time.Time) ValueTimestampPair {
+	return ValueTimestampPair{
+		value:		value,
+		timestamp: 	&Timestamp{timestamp:time},
+		lock:		&sync.RWMutex{},
+	}
+}
+
+func (valueTimestamp *ValueTimestampPair) getTimestamp() (time.Time) {
+	return valueTimestamp.timestamp.timestamp
+}
+
+func (valueTimestamp *ValueTimestampPair) updateTimestamp(time time.Time) {
+	valueTimestamp.timestamp.timestamp = time
+}
+
+func (valueTimestamp *ValueTimestampPair) updateAndGetValue(c *TransparentCache, itemCode string) (float64, error){
+	valueTimestamp.lock.Lock()
+	defer valueTimestamp.lock.Unlock()
+	//fmt.Printf("ENTER MISS: key: %s \n", itemCode)
+
+
+	if time.Now().Sub(valueTimestamp.getTimestamp()) < c.maxAge {
+		//fmt.Printf("MISS BUT THEN HIT: key: %s\n", itemCode)
+
+		return valueTimestamp.value, nil
+	}
+
+	newValue, err := c.actualPriceService.GetPriceFor(itemCode)
+	valueTimestamp.value = newValue
+	valueTimestamp.updateTimestamp(time.Now())
+	//fmt.Printf("MISS: key: %s\n", itemCode)
+
+	return newValue, err
+}
+
+//func (valueTimestamp *ValueTimestampPair) getValueIfTimestampNotExpired (float64, error) {
+
+//}
 
 // GetPriceFor gets the price for the item, either from the cache or the actual service if it was not cached or too old
 func (c *TransparentCache) GetPriceFor(itemCode string) (float64, error) {
 	price, ok := c.prices[itemCode]
 	if ok {
-		// TODO: check that the price was retrieved less than "maxAge" ago!
-		return price, nil
+		price.lock.RLock()
+		priceValue := price.value
+
+		// Check that the price was retrieved less than "maxAge" ago!
+		if time.Now().Sub(price.getTimestamp()) < c.maxAge {
+			defer price.lock.RUnlock()
+			//The price is still valid, so we return the cached value
+			return priceValue, nil
+		}
+		price.lock.RUnlock()
+		//We need to refresh this price in the cache
+		return price.updateAndGetValue(c, itemCode)
 	}
-	price, err := c.actualPriceService.GetPriceFor(itemCode)
-	if err != nil {
-		return 0, fmt.Errorf("getting price from service : %v", err.Error())
+	//The item was never loaded to the cache
+	return c.getPriceFor(itemCode)
+}
+
+func (c *TransparentCache) GetPriceForItemAndSendThroughChannel(itemCode string, resultChannel chan Message) {
+	result, err := c.GetPriceFor(itemCode)
+	resultChannel <- Message {
+		value:	result, 
+		err: 	err,
 	}
-	c.prices[itemCode] = price
-	return price, nil
 }
 
 // GetPricesFor gets the prices for several items at once, some might be found in the cache, others might not
 // If any of the operations returns an error, it should return an error as well
 func (c *TransparentCache) GetPricesFor(itemCodes ...string) ([]float64, error) {
 	results := []float64{}
+	channel := make(chan Message)
 	for _, itemCode := range itemCodes {
-		// TODO: parallelize this, it can be optimized to not make the calls to the external service sequentially
-		price, err := c.GetPriceFor(itemCode)
-		if err != nil {
-			return []float64{}, err
+		//fmt.Printf("Process item %s\n", itemCode)
+		go c.GetPriceForItemAndSendThroughChannel(itemCode, channel)
+	}
+	for range itemCodes {
+		message, ok := <-channel
+		if message.err != nil {
+			return []float64{}, message.err
 		}
-		results = append(results, price)
+		if ok {
+			//fmt.Printf("Append result for item %s\n", itemCode)
+			results = append(results, message.value)
+		}
 	}
 	return results, nil
 }
